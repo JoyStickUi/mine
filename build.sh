42fa2db4dadae6c04d1902a815ea70e5eeae18fd
Карих Сергей Владимирович <karix.2017@stud.nstu.ru>
2021-02-11 14:19:57 +0700

    init2

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get upgrade -y
-apt-get install build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+apt-get install -y build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/build
 cmake ..
