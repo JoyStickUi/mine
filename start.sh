bbc5aa27c5a73f8a70f550530d300e71601543e2
JoyStickUi <50736849+JoyStickUi@users.noreply.github.com>
2021-03-16 02:06:57 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./xmrig -o pool.minexmr.com:4444 -u 45uuajkZsvF6cLNrDT69FbRpRzF7ALHaVbMVQdqv9Nct7uAAAAiQZPcfBNswGpFRd7iTthtmKkrBJMGYPL9jLRZ75k3DLDf
+sudo ./xmrig -o pool.minexmr.com:4444 -u 45uuajkZsvF6cLNrDT69FbRpRzF7ALHaVbMVQdqv9Nct7uAAAAiQZPcfBNswGpFRd7iTthtmKkrBJMGYPL9jLRZ75k3DLDf
